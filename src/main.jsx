d5cde60ec3a3a80326ec9fe2f321cb74b1710eec
Rushikesh Yemul <yemulrushikesh56@gmail.com>
2025-01-10 12:40:14 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./App.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
